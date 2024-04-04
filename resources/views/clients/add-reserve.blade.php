cd092ae5b8bdb2ce3e0917f25d5d6b17bc845b62
Patrick Ivan Sundiam <sundiam.patrickivan.12032002@gmail.com>
2024-04-04 17:51:33 +0800

    progress on reservation form views

--- resources/views/clients/add-reserve.blade.php
+++ resources/views/clients/add-reserve.blade.php
@@ -4,14 +4,21 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-5xl sm:px-6 lg:px-8">
             <div class="p-5 overflow-hidden bg-white shadow-xl md:p-10 dark:bg-gray-800 sm:rounded-lg">
 
                 <form action="{{ route('reservation.store') }}" 
                     method="post" 
                     enctype="multipart/form-data">
                 @csrf
-                    <x-label class="!text-lg">
+
+                    <div class="flex items-center flex-col">
+                        <h2 class="forms-heading-text">Reservation Details</h2>
+                    </div>
+
+                    <x-form-divider value="Personal Information & Event Details" />
+
+                    {{-- <x-label class="!text-lg">
                         Select a package
                         <x-input-error for="package" />
                     </x-label>
@@ -43,42 +50,53 @@
                                 </div>
                             </label>
                         </li>
-                    </ul>
+                    </ul> --}}
 
-                    <div class="grid w-full grid-cols-1 mx-auto mt-5 md:grid-cols-2 gap-x-4 lg:w-1/2">
+                    <div class="grid w-full grid-cols-1 mx-auto md:grid-cols-2 gap-x-16">
                         <div class="mt-5">
-                            <x-label for="address" required>Address:</x-label>
-                            <textarea name="address" class="w-full border-gray-300 rounded-lg focus:border-primary focus:ring-primary-hover" id="">{{ old('address') }}</textarea>
-                            <x-input-error for="address" />
+                            <x-label for="name" required>Name:</x-label>
+                            <x-input type="text" name="name" class="w-full" id="" value="{{ auth()->user()->name }}" />
+                            <x-input-error for="name" />
                         </div>
 
                         <div class="mt-5">
                             <x-label for="theme" required>Theme:</x-label>
-                            <x-input type="text" name="theme" class="w-full" id="" :value="old('theme')" />
+                            <select name="theme" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Theme') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                                <option value="Gold">{{ __('Gold Party') }}</option>
+                                <option value="Kanto">{{ __('Kanto Style') }}</option>
+                                <option value="Disney">{{ __('Disney') }}</option>
+                            </select>
                             <x-input-error for="theme" />
+                        </div>
+
+                        <div class="mt-5">
+                            <x-label for="address" required>Address:</x-label>
+                            <input name="address" class="w-full input-field" id="" value="{{ auth()->user()->address }}" /> 
+                            <x-input-error for="address" />
                         </div>
                         
                         <div class="mt-5">
                             <x-label for="occasion" required>Occasion:</x-label>
-                            <select name="occasion" class="w-full border-gray-300 rounded-lg focus:border-primary focus:ring-primary-hover" id="">
+                            <select name="occasion" class="w-full input-field" id="">
                                 <option selected disabled>{{ __('Select Occasion') }}</option>
-                                <option value="Wedding">{{ __('Wedding') }}</option>
-                                <option value="Debut">{{ __('Debut') }}</option>
-                                <option value="Dinner">{{ __('Dinner') }}</option>
-                                <option value="Lunch">{{ __('Lunch') }}</option>
-                                <option value="Kiddie">{{ __('Kiddie') }}</option>
+                                <option value="Wedding ">{{ __('Wedding ') }}</option>
+                                <option value="Debut">{{ __('Debut Party') }}</option>
+                                <option value="Christening">{{ __('Christening') }}</option>
+                                <option value="Anniversarry">{{ __('Anniversarry') }}</option>
+                                <option value="Birthday">{{ __('Birthday') }}</option>
                             </select>
                             <x-input-error for="occasion" />
                         </div>
-
-                        <div class="mt-5">
-                            <x-label for="time" required>Time:</x-label>
-                            <x-input type="time" name="time" class="w-full" id="" :value="old('time')" />
-                            <x-input-error for="time" />
-                        </div>
                     </div>
 
+                    <x-form-divider value="Time and Date" />
+
                     <div class="w-full mx-auto mt-5 lg:w-2/3">
+                        <x-label for="time" required>Time:</x-label>
+                        <x-input type="time" name="time" class="w-full" id="" :value="old('time')" />
+                        <x-input-error for="time" />
                         <x-label for="date" required>
                             Choose date:
                         </x-label>
@@ -86,14 +104,87 @@
                         <x-input-error for="date" />
                     </div>
 
-                    {{-- <div class="flex flex-col w-full mx-auto mt-5 md:w-1/2">
+                    <x-form-divider value="Package Details" />
 
-                        <x-label for="id-verify-img" required>Id Verification Photo:</x-label>
-                        <x-dropbox id="id-verify-img" label="Click to upload" name="id-verify-img"/>
-                        <x-input-error for="id-verify-img" />
-                    </div> --}}
+                    <div class="grid w-full grid-cols-1 mx-auto md:grid-cols-2 gap-x-16">
+                        <div class="mt-5">
+                            <x-label for="package" required>Package:</x-label>
+                            <select name="package" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Package') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="package" />
+                        </div>
+                        
+                        <div class="mt-5">
+                            <x-label for="meat" required>Meat:</x-label>
+                            <select name="meat" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Meat') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="meat" />
+                        </div>
 
-                    <div class="flex flex-col w-full mx-auto my-5 md:w-1/2">
+                        <div class="mt-5">
+                            <x-label for="dishes" required>Dishes:</x-label>
+                            <select name="dishes" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Dishes') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="dishes" />
+                        </div>
+
+                        <div class="mt-5">
+                            <x-label for="side_dish" required>Side Dish:</x-label>
+                            <select name="side_dish" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Side Dish') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="side_dish" />
+                        </div>
+
+                        <div class="mt-5">
+                            <x-label for="appetizer" required>Appetizer:</x-label>
+                            <select name="appetizer" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Appetizer') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="appetizer" />
+                        </div>
+
+                        <div class="mt-5">
+                            <x-label for="dessert" required>Dessert:</x-label>
+                            <select name="dessert" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Dessert') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="dessert" />
+                        </div>
+
+                        <div class="mt-5">
+                            <x-label for="beverages" required>Beverages:</x-label>
+                            <select name="beverages" class="w-full input-field" id="">
+                                <option selected disabled>{{ __('Select Beverages') }}</option>
+                                <option value="Formal ">{{ __('Formal ') }}</option>
+                            </select>
+                            <x-input-error for="beverages" />
+                        </div>
+                    </div>
+
+
+                    <x-form-divider value="Payment Details" />
+
+                    <div class="flex flex-col w-full mx-auto my-5 lg:w-2/3">
+                        <x-label for="payment_percent" required>Payment Percent:</x-label>
+                        <select name="payment_percent" class="w-full input-field" id="">
+                            <option selected disabled>{{ __('Select Payment Percent') }}</option>
+                            <option value="40">{{ __('40') }}</option>
+                            <option value="50">{{ __('50') }}</option>
+                            <option value="75">{{ __('75') }}</option>
+                            <option value="75">{{ __('75') }}</option>
+                            <option value="100">{{ __('Full') }}</option>
+                        </select>
+                        <x-input-error for="payment_percent" />
 
                         <x-label for="receipt-img" required>Receipt Photo:</x-label>
                         <x-dropbox id="receipt-img" label="Click to upload" name="receipt-img"/>
---
+++ resources/views/components/form-divider.blade.php
@@ -0,0 +1,7 @@
+@props(['value'])
+
+<div class="flex items-center justify-center w-full my-5">
+    <div class="h-[2px] bg-gray-500 grow"></div>
+    <h2 class="mx-4 text-md md:text-xl font-noticia">{{ $value }}</h2>
+    <div class="h-[2px] bg-gray-500 grow"></div>
+</div>
