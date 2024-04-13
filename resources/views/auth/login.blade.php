c388807bfce2178a669fe1b971bd04418f0b1490
Patrick Ivan Sundiam <sundiam.patrickivan.12032002@gmail.com>
2024-04-13 22:41:20 +0800

    added loading states on submit buttons to prevent potential errors.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,9 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('login') }}"
+            x-data="{ buttonDisabled: false }"
+            x-on:submit="buttonDisabled = true">
             @csrf
 
             <div class="flex items-center flex-col mb-5">
@@ -44,8 +46,11 @@
                     </a>
                 @endif
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
+                <x-button class="ms-4 min-w-20" x-bind:disabled="buttonDisabled">
+                    <div role="status" x-show="buttonDisabled" class="w-full">
+                        <svg class="mx-auto animate-spin" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612" stroke="#e2e8f0" stroke-width="3.55556" stroke-linecap="round"></path> </g></svg>
+                    </div>
+                    <span x-show="!buttonDisabled">{{ __('Log in') }}</span>
                 </x-button>
             </div>
         </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,9 @@
 
         <form method="POST" 
             action="{{ route('register') }}" 
-            enctype="multipart/form-data">
+            enctype="multipart/form-data"
+            x-data="{ buttonDisabled: false }"
+            x-on:submit="buttonDisabled = true">
             @csrf
 
             <div class="flex items-center flex-col mb-5">
@@ -103,8 +105,11 @@
                             {{ __('Already registered?') }}
                         </a>
         
-                        <x-button class="ms-4">
-                            {{ __('Register') }}
+                        <x-button class="ms-4 min-w-24" x-bind:disabled="buttonDisabled">
+                            <div role="status" x-show="buttonDisabled" class="w-full">
+                                <svg class="mx-auto animate-spin" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612" stroke="#e2e8f0" stroke-width="3.55556" stroke-linecap="round"></path> </g></svg>
+                            </div>
+                            <span class="mx-auto" x-show="!buttonDisabled">{{ __('Sign Up') }}</span>
                         </x-button>
                     </div>
                 </div>
