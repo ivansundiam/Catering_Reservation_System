49b6cfa1ba3120adf5ee2ccf282473b110af0b7c
Patrick Ivan Sundiam <sundiam.patrickivan.12032002@gmail.com>
2024-04-29 23:53:22 +0800

    added mail notification in admin when a reservation has been made

--- app/Listeners/ReservationCompleteListener.php
+++ app/Listeners/ReservationCompleteListener.php
@@ -4,11 +4,13 @@ namespace App\Listeners;
 
 use App\Events\ReservationComplete;
 use App\Mail\ReservationReceiptMail;
+use App\Mail\ReservationCompleteMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use App\Models\Reservation;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class ReservationCompleteListener
 {
@@ -26,5 +28,6 @@ class ReservationCompleteListener
     public function handle(ReservationComplete $event): void
     {
         Mail::to($event->user->email)->send(new ReservationReceiptMail($event->user, $event->reservation));
+        Mail::to(User::admin()->email)->send(new ReservationCompleteMail($event->user, $event->reservation));
     }
 }
---
+++ app/Mail/ReservationCompleteMail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+use App\Models\Reservation;
+use Illuminate\Mail\Mailables\Address;
+
+class ReservationCompleteMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $reservation;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(User $user, Reservation $reservation)
+    {
+        $this->user = $user;
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Reservation Added',
+            from: new Address(User::admin()->email, 'Robert Camba'),
+
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.reservation-complete',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
