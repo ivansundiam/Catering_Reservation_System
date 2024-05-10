233027e6631d020fdf73e9fa57333c132503d6e0
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2024-05-11 01:18:58 +0800

    added automatic removal of reservations with notice for the past 3 days

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('reservations:remove-has-notice')->everyFourHours();
     }
 
     /**
@@ -29,4 +30,6 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    
 }
