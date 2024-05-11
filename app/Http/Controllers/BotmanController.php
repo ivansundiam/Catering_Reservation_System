23b7d7fc19aa2accb8241afd6e67c633d273c959
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2024-05-11 16:31:18 +0800

    added botman promts

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -41,10 +41,28 @@ class BotmanController extends Controller
                     $bot->reply('Sure! Here are some commands you can try:');
                     break;
 
+                case 'can i see the menu?':
+                case 'services':
+                $bot->reply('Our menu is located in the "Services" section. <a href="/#services" target="_parent">Click this link</a> to see services.');
+                break;
+
+                case 'where are you located?':
+                case 'location':
+                $bot->reply("Robert Camba's catering services is located in Unit 107 Westeria Residences 77 west ave, Quezon City, Philippines");
+                break;
+
+                case 'what time are the operating hours?':
+                case 'time':
+                $bot->reply('Our reservation hours are from <u>8:00 AM to 8:00 PM</u>. Feel free to reserve during this time.');
+                break;
+
                 default: 
                     $question = Question::create('Sorry, I did not understand these commands. Here is a list of commands I understand:');
                     $question->addButtons([
                         Button::create('"Hi" or "Hello"')->value('hi'),
+                        Button::create('Can I see the menu?')->value('services'),
+                        Button::create('Where are you located?')->value('location'),
+                        Button::create('What time are the operating hours?')->value('time'),
                         Button::create('How to make a reservation')->value('reserve'),
                         Button::create('How to sign in')->value('register'),
                         Button::create('What are the services')->value('services'),
